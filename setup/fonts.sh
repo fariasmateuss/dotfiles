4719a76df3c7bca410c9b927aa5cfad8686580df
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-01-18 11:54:57 -0400

    Add script to install fonts

---
+++ setup/fonts.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+declare -a fonts=(
+  VictorMono
+  JetBrainsMono
+)
+
+version='2.1.0'
+fonts_dir="${HOME}/.local/share/fonts"
+
+if [[ ! -d "$fonts_dir" ]]; then
+    mkdir -p "$fonts_dir"
+fi
+
+for font in "${fonts[@]}"; do
+    zip_file="${font}.zip"
+    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
+    echo "Downloading $download_url"
+    wget "$download_url"
+    unzip "$zip_file" -d "$fonts_dir"
+    rm "$zip_file"
+done
+
+find "$fonts_dir" -name '*Windows*' -delete
+
+fc-cache -fv
+
+echo 'Fonts installed successfully!'
